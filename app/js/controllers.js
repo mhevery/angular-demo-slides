d1ac04aacf038a487746f0a3ef596133c9cf8798
Misko Hevery <misko@hevery.com>
2011-03-01 14:11:04 -0800

    rewritten the new intro

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -22,34 +22,41 @@ function PresentationCntrl($route, $window) {
         {seq:slideCount, template:'partials/' + partial + '.html', title:title});
   }
 
-  slide('html_is_broken',    'HTML is broken');
-  slide('knowledge',         'What you need to build a web-app');
-  slide('jQuery',            'jQuery solves DOM manipulation');
+  slide('intro/00',            '');
+  slide('intro/01',            'What\'s wrong with HTML');
+  slide('intro/02',            'HTML');
+  slide('intro/03',            'JavaScript');
+  slide('intro/04',            'AJAX / Web 2.0');
+  slide('intro/05',            'Lack of Abstraction');
 
-  slide('dom_manipulation',  'Manipulating DOM');
-  slide('input',             'Reading input');
-  slide('password',          'Application Behavior');
-  slide('password_xhr',      'XHR');
-
-  slide('overview',          'Overview');
-  slide('template',          'View as a projection of model');
-  slide('filters',           'Rendering your data');
-  slide('directives',        'Directives');
-  slide('lifecycle',         'Lifecycle');
-  slide('services1',         'Basic building blocks of web-app');
-  slide('services2',         'Basic building blocks of web-app');
-  slide('di',                'Dependency injection');
-  slide('unit-testing',      'Unit testing');
-  slide('e2e-testing',       'Ende-to-end testing');
-
-  slide('app_logic',         "Focus on what matters");
-  slide('scafolding',        "Reduce Chaos");
-  slide('learning_curve',    "Shorten Learning Curve");
-  slide('maintenance',       "Reduce Maintainance");
-  slide('development_speed', "Development Speed");
-  slide('server',            "Simplify server implementation");
-
-  slide('difference',        "It's different");
-  slide('angular',           'A fresh start');
+//  slide('html_is_broken',    'HTML is broken');
+//  slide('knowledge',         'What you need to build a web-app');
+//  slide('jQuery',            'jQuery solves DOM manipulation');
+//
+//  slide('dom_manipulation',  'Manipulating DOM');
+//  slide('input',             'Reading input');
+//  slide('password',          'Application Behavior');
+//  slide('password_xhr',      'XHR');
+//
+//  slide('overview',          'Overview');
+//  slide('template',          'View as a projection of model');
+//  slide('filters',           'Rendering your data');
+//  slide('directives',        'Directives');
+//  slide('lifecycle',         'Lifecycle');
+//  slide('services1',         'Basic building blocks of web-app');
+//  slide('services2',         'Basic building blocks of web-app');
+//  slide('di',                'Dependency injection');
+//  slide('unit-testing',      'Unit testing');
+//  slide('e2e-testing',       'Ende-to-end testing');
+//
+//  slide('app_logic',         "Focus on what matters");
+//  slide('scafolding',        "Reduce Chaos");
+//  slide('learning_curve',    "Shorten Learning Curve");
+//  slide('maintenance',       "Reduce Maintainance");
+//  slide('development_speed', "Development Speed");
+//  slide('server',            "Simplify server implementation");
+//
+//  slide('difference',        "It's different");
+//  slide('angular',           'A fresh start');
 }
 
