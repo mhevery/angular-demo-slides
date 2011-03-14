90f9663b63fd7ff408c9e9995c3f2678ec86edd7
Misko Hevery <misko@hevery.com>
2011-03-13 20:34:35 -0700

    remove all slides from master so that they can be moved to branch

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -24,47 +24,6 @@ function PresentationCntrl($route, $window) {
   }
 
   slide('0_intro/00',   '');
-  slide('0_intro/01',   'What\'s wrong with HTML');
-  slide('0_intro/02',   'HTML');
-  slide('0_intro/03',   'JavaScript');
-  slide('0_intro/04',   'AJAX / Web 2.0');
-  slide('0_intro/05',   'Lack of Abstraction');
-
-  slide('1_html/00',    'But what about ...');
-  slide('1_html/01',    'Hello Document');
-  slide('1_html/02',    'Hello JavaScript');
-  slide('1_html/03',    'Hello jQuery');
-  slide('1_html/04',    'Hello <angular/>');
-  slide('1_html/05',    'Hello <angular/> MVC');
-
-  slide('2_form/00',    'Hello form');
-  slide('2_form/01',    'Hello form');
-  slide('2_form/02',    'Hello form');
-  slide('2_form/03',    'Hello form');
-  slide('2_form/04',    'jQuery???');
-  slide('2_form/05',    'jQuery <3');
-  slide('2_form/06',    'web-apps > DOM manipulation');
-
-  slide('3_angular/00', 'increase the abstraction');
-  slide('3_angular/01', 'XHR');
-  slide('3_angular/02', 'Controllers and DI');
-
-  slide('4_testing/00', 'Testing Behavior');
-  slide('4_testing/01', 'How Testable?');
-  slide('4_testing/02', 'Separation of Concerns');
-  slide('4_testing/03', 'Dependency Injection');
-  slide('4_testing/04', 'Test Harness');
-
-  slide('5_angular/00', 'Implementation');
-  slide('5_angular/01', 'Abstraction: $route');
-  slide('5_angular/02', 'Abstraction: $resource');
-  slide('5_angular/03', 'Abstraction: Input Validation');
-  slide('5_angular/04', 'Abstraction: Filters and Formatters');
-  slide('5_angular/05', 'Abstraction: Your DSL');
-  slide('5_angular/06', 'HTML is here to stay');
-  slide('5_angular/07', 'Angular Summary');
-  slide('5_angular/08', 'We are not done yet');
-  slide('5_angular/09', 'The End');
 
 }
 
